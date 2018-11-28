de8a25728cd32423c70bad3e8d52c6e5abbb6898
RonaldSantosR <rsantos@exact.com.pe>
2018-11-28 14:42:26 -0500

    Creacion de proyecto

---
+++ src/main/java/com/exact/proveedores/ServiceProveedoresApplication.java
@@ -0,0 +1,12 @@
+package com.exact.proveedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProveedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProveedoresApplication.class, args);
+	}
+}
